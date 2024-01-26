4284cc0534d4cad04f0d724fb084246581f68510
ADXID12 <131492273+ADXID12@users.noreply.github.com>
2024-01-26 15:21:31 -0600

    Add files via upload

--- Lab2P2_GabrielOsorto/Lab2P2_GabrielOsorto/src/lab2p2_gabrielosorto/Lab2P2_GabrielOsorto.java
+++ Lab2P2_GabrielOsorto/Lab2P2_GabrielOsorto/src/lab2p2_gabrielosorto/Lab2P2_GabrielOsorto.java
@@ -13,6 +13,7 @@ public class Lab2P2_GabrielOsorto {
     static Scanner lt = new Scanner(System.in);
 
     public static void main(String[] args) {
+        //crear usuarios predeterminados para ingresar 
         String Username = "Carlos";
         String Contra = "Queonda";
         String TipoUsuario = "estudiante";
@@ -30,6 +31,7 @@ public class Lab2P2_GabrielOsorto {
         TipoUsuario = "bibliotecario";
         Usuario user3 = new Usuario(Username, Contra, TipoUsuario);
         usuarios.add(user3);
+        //fin creacion usuarios
 
         System.out.println("Ingresar Cuenta");
         System.out.print("Ingrese su nombre de Usuario:");
@@ -42,15 +44,58 @@ public class Lab2P2_GabrielOsorto {
                     String TypeUser = usuarios.get(i).getTipoUsuario();
                     switch (TypeUser) {
                         case "estudiante":
-                            ListarRecursos(libros, articulos, cursos, conferencias);
+                            boolean seguirEstudiante = true;
+                            while (seguirEstudiante == true) {
+                                System.out.println("Menu Estudiantes:");
+                                System.out.println("1. Listar recursos");
+                                System.out.println("2. Salir");
+                                System.out.print("Que desea hacer:");
+                                int opEstudiante = sc.nextInt();
+                                switch (opEstudiante) {
+                                    case 1:
+                                        System.out.println("Lista de recursos");
+                                        ListarRecursos(libros, articulos, cursos, conferencias);
+                                        break;
+                                    case 2:
+                                        System.out.println("Saliendo....");
+                                        seguirEstudiante = false;
+                                        break;
+                                    default:
+                                        System.out.println("OPCION INEXISTENTE");
+                                        break;
+                                }//fin switch case menu estudiante
+                            }//fin while menu estudiante
                             break;
+                        //FIN CASE ESTUDIANTE
+
                         case "profesor":
-                            System.out.println("La concha de tu madre bob esponja");
+                            boolean seguirProfe = true;
+                            while (seguirProfe == true) {
+                                System.out.println("Menu Profesor");
+                                System.out.println("1. Listar recursos");
+                                System.out.println("2. Agregar Recursos");
+                                System.out.println("3. Salir");
+                                System.out.print("Que desea hacer?:");
+                                int opMaestro = sc.nextInt();
+                                switch (opMaestro) {
+                                    case 1:
+                                        break;
+                                    case 2:
+                                        break;
+                                    case 3:
+                                        break;
+                                    default:
+                                        break;
+
+                                }
+                            }
                             break;
+                        //FIN CASE PROFESOR
                         case "bibliotecario":
                             System.out.println("Ya ya mijo ya");
                             break;
                     }
+                    //FIN CASE BIBLIOTECARIO
                 } else {
                     System.out.println("Contraseña Incorrecta :(");
                 }
@@ -58,13 +103,30 @@ public class Lab2P2_GabrielOsorto {
                 System.out.println("Usuario Inexistente :(");
             }
         }
-    }
+    }//fin del main
 
-    public static void ListarRecursos(ArrayList<Libros> libros, ArrayList<ArtiCULOS> articulos, ArrayList<CursosenLinea> cursos, ArrayList<ConferenciasVirtuales> conferencias) {
-        for (int i = 0; i < libros.size(); i++) {
-            Libros Books = libros.get(i);
+    public static void ListarRecursos(ArrayList<Libros> librosArray, ArrayList<ArtiCULOS> articulosArray, ArrayList<CursosenLinea> cursosArray, ArrayList<ConferenciasVirtuales> conferenciasArray) {
+        for (int i = 0; i < librosArray.size(); i++) {
+            Libros Books = librosArray.get(i);
             System.out.print(Books);
-            
         }
-    }
-}
+        for (int i = 0; i < articulosArray.size(); i++) {
+            ArtiCULOS arti = articulosArray.get(i);
+            System.out.println(arti);
+        }
+        for (int i = 0; i < cursosArray.size(); i++) {
+            CursosenLinea cursoslista = cursosArray.get(i);
+            System.out.println(cursoslista);
+        }
+        for (int i = 0; i < conferenciasArray.size(); i++) {
+            ConferenciasVirtuales confe = conferenciasArray.get(i);
+            System.out.println(confe);
+        }
+    }//fin del metodo imprimir recursos
+
+    public static void AgregarRecursos(ArrayList<Libros> librosArray, ArrayList<ArtiCULOS> articulosArray, ArrayList<CursosenLinea> cursosArray, ArrayList<ConferenciasVirtuales> conferenciasArray) {
+        System.out.println("Agregar Recursos:");
+        System.out.println("Que tipo de recurso desea agregar?");
+    }//fin metodo AgregarRecursos
+}//fin de la clase
+
